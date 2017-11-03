bbbca80eace9f131afc9ea4f3f3543708ffb25cf
Tim Vidas <tvidas@alumni.cmu.edu>
2017-11-02 21:45:56 -0500

    adds Dec 2017 talk

--- source/Schedule/Schedule.js
+++ source/Schedule/Schedule.js
@@ -50,6 +50,19 @@ export default class Schedule extends Component {
         <ContentBoxParagraph>
           Adam Schaal will be discussing web-based offensive security and how these tools can be used to educate developers.
         </ContentBoxParagraph>
+
+        <ContentBoxHeader
+          text='Tuesday, December 6th - DEF CON 402 Meetup'
+          location='Pizza West'
+          map='https://goo.gl/maps/6AzW5Bi4bvQ2'
+          time='4-6:00pm'
+          paramStyle={styles.Header}
+        />
+
+        <ContentBoxParagraph>
+          Tim Vidas will be presenting the DARPA Cyber Grand Challnge, in pictures.  Bring your 3D glasses.
+        </ContentBoxParagraph>
+
         
       </ContentBox>
     );
